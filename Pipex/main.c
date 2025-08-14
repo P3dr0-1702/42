e3c39790c08e115b50ebd5ca78ae43b2939c97f4
Pedro Augusto Freire Costa <pfreire-@c2r9s4.42lisboa.com>
2025-08-14 18:23:51 +0100

    Fixed a bit of Pipex

--- Pipex/main.c
+++ Pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: pfreire- <pfreire-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 14:45:53 by pfreire-          #+#    #+#             */
-/*   Updated: 2025/08/12 17:02:27 by pfreire-         ###   ########.fr       */
+/*   Updated: 2025/08/14 18:13:36 by pfreire-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,46 +83,72 @@ void	pipexx(t_pipex *pipex)
 	free_pipex(pipex);
 }
 
-void	init_null(t_pipex *pipex)
+char	*command_to_str(char **path, char *command)
 {
-	ft_bzero(pipex, sizeof(t_pipex));
+	int		i;
+	int		fd;
+	char	*path_slash;
+	char	*absolute_path;
+
+	i = 0;
+	fd = 0;
+	while (path[i] != NULL)
+	{
+		path_slash = ft_strjoin(path[i], "/");
+		absolute_path = ft_strjoin(path_slash, command);
+		fd = access(absolute_path, F_OK);
+		if (fd == 0)
+		{
+			free(path_slash);
+			return (absolute_path);
+		}
+		i++;
+		free(path_slash);
+		free(absolute_path);
+	}
+	fd = access(command, F_OK);
+	if(fd == 0)
+		return(command);
+	return(NULL);
 }
 
 int	main(int argc, char **argv, char **env)
 {
 	t_pipex	pipex;
-	char	*str;
-	char	*str2;
-	int		str_1;
-	int		str_2;
+	// char	*str;
+	// char	*str2;
+	// int		str_1;
+	// int		str_2;
 
 	if (argc != 5)
 		return (ft_printf("Not those args\n"), -1);
-	init_null(&pipex);
+	ft_bzero(&pipex, sizeof(t_pipex));
 	pipex.cmd1 = ft_split(argv[2], ' ');
 	pipex.cmd2 = ft_split(argv[3], ' ');
 	pipex.infile_fd = open(argv[1], O_RDONLY);
 	if (pipex.infile_fd < 0)
 		return (ft_printf("bash: no such file or directory: %s\n", argv[1]), 1);
 	pipex.path = pathfinder(env);
-	str_1 = command_exist(pipex.cmd1[0], pipex.path);
-	str = NULL;
-	if (str_1 >= 0)
-		str = ft_strjoin(pipex.path[str_1], "/");
-	str_2 = command_exist(pipex.cmd2[0], pipex.path);
-	if(str_2 >= 0)
-		str2 = ft_strjoin(pipex.path[str_2], "/");
-	if(str_2 == -1)
-	{
-		free(str);
-		free_pipex(&pipex);
-		exit(127);
-	}
-	pipex.absolute_path1 = ft_strjoin(str, pipex.cmd1[0]);
-	pipex.absolute_path2 = ft_strjoin(str2, pipex.cmd2[0]);
+	// str_1 = command_exist(pipex.cmd1[0], pipex.path);
+	// str = NULL;
+	// if (str_1 >= 0)
+	// 	str = ft_strjoin(pipex.path[str_1], "/");
+	// str_2 = command_exist(pipex.cmd2[0], pipex.path);
+	// if (str_2 >= 0)
+	// 	str2 = ft_strjoin(pipex.path[str_2], "/");
+	// if (str_2 == -1)
+	// {
+	// 	free(str);
+	// 	free_pipex(&pipex);
+	// 	exit(127);
+	// }
+	// ft_printf("%s\n", str);
+	// ft_printf("%s\n", str2);
+	pipex.absolute_path1 = command_to_str(pipex.path, pipex.cmd1[0]);
+	pipex.absolute_path2 = command_to_str(pipex.path, pipex.cmd2[0]);
 	pipex.outfile_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	free(str);
-	free(str2);
+	// free(str);
+	// free(str2);
 	pipex.env = env;
 	pipe(pipex.fd);
 	// if (command_exist(pipex.cmd1[0], pipex.path))
